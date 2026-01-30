71dc0e161cd3ed3627770bccdf6316d93addb8a1
General Lennobi <69532790+lennehberg@users.noreply.github.com>
2026-01-30 17:59:57 +0200

    Player controller w items pick ups (#5)
    * Basic player controlls
    
    * Input System for handling controls
    * Basic responsive player controls
    * Ground checker for jumps
    
    * implemented item and item data blueprint
    
    * resolved merge conflicts
    
    * added logic to pick up items
    
    * items are now pickupable
    
    * player collider ignores world items
    
    * saved the scene changes
    
    ---------
    
    Co-authored-by: Adi Zahavi <adidizahavi5@gmail.com>

---
+++ Assets/Scripts/Player Controller/PlayerMovementPlatformer.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private GroundChecker groundCheck;
+    
+    private Rigidbody2D _rb2d;
+    private InputAction _moveAction;
+    private InputAction _jumpAction;
+    
+    private void Awake()
+    {
+        _rb2d = GetComponent<Rigidbody2D>();
+    }
+    
+    private void Start()
+    {
+        _moveAction = InputSystem.actions.FindAction("Move");
+        _jumpAction = InputSystem.actions.FindAction("Jump");
+    }
+
+    // fixed update better for physics calculations
+    private void FixedUpdate()
+    {
+        // side movement
+        var horizontalSpeed = _moveAction.ReadValue<Vector2>().x * speed;
+        var currentYSpeed = _rb2d.linearVelocity.y; // keeping gravity and jumps
+        _rb2d.angularVelocity = 0f;
+        _rb2d.linearVelocity = new Vector2(horizontalSpeed, currentYSpeed);
+    }
+
+    // normal tick update better for input handling
+    private void Update()
+    {
+        // jumping
+        if (_jumpAction.triggered && groundCheck.IsGrounded)
+        {
+            _rb2d.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
