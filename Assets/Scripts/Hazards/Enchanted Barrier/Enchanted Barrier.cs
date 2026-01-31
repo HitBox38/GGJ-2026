66c602de220daf499d7f9e8bb0c6e9e514e7757b
lennehberg <lenny.agizim@gmail.com>
2026-01-31 15:58:05 +0200

    rabbit mask works pretty nicely with barriers and cauldron

--- Assets/Scripts/Hazards/Enchanted Barrier/Enchanted Barrier.cs
+++ Assets/Scripts/Hazards/Enchanted Barrier/Enchanted Barrier.cs
@@ -7,7 +7,7 @@ public class EnchantedBarrier : MonoBehaviour
 
     private float _originalMovementSpeed;
     
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerStay2D(Collider2D other)
     {
         if (!other.CompareTag("Player")) return;
         other.GetComponent<PlayerMovement>().SetSpeedModifier(speedReducingPercentage);
@@ -16,6 +16,13 @@ public class EnchantedBarrier : MonoBehaviour
     private void OnTriggerExit2D(Collider2D other)
     {
         if (!other.CompareTag("Player")) return;
+        // if player has a rabbit mask, set the speed modifier to the mask's modifer
+        RabbitMask rabbitMask = other.GetComponentInChildren<RabbitMask>();
+        if (rabbitMask != null)
+        {
+            other.GetComponent<PlayerMovement>().SetSpeedModifier(rabbitMask.speedBoostAmount);
+            return;
+        }
         other.GetComponent<PlayerMovement>().SetSpeedModifier(1);
     }
 }
