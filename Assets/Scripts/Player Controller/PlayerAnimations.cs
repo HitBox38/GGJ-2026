48831a2a26946e46535de525249c2a7264ae4c9d
Adi Zahavi <adidizahavi5@gmail.com>
2026-01-31 03:43:15 +0200

    Player Animations Setup
    Animations controller and setup for the player aswell as sprite image direction according to the move direction.

---
+++ Assets/Scripts/Player Controller/PlayerAnimations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerAnimations : MonoBehaviour
+{
+    [SerializeField] private Transform spriteHolder;
+
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
+    
+    private InputAction _moveAction;
+    
+    private void Awake()
+    {
+        _animator = spriteHolder.GetComponent<Animator>();
+        _spriteRenderer = spriteHolder.GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        _moveAction = InputSystem.actions.FindAction("Move");
+    }
+
+    private void Update()
+    {
+        var horizontalMovement = _moveAction.ReadValue<Vector2>().x;
+        if (!Mathf.Approximately(horizontalMovement, 0)) 
+            _spriteRenderer.flipX = horizontalMovement < 0;
+    }
+}
