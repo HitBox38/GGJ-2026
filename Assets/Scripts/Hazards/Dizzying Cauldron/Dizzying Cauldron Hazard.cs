d02f69c8b95296673aa166c5022e22a109f3e352
lennehberg <lenny.agizim@gmail.com>
2026-01-31 15:07:12 +0200

    added rabbit mask

--- Assets/Scripts/Hazards/Dizzying Cauldron/Dizzying Cauldron Hazard.cs
+++ Assets/Scripts/Hazards/Dizzying Cauldron/Dizzying Cauldron Hazard.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class DizzyingCauldronHazard : MonoBehaviour
 {
-    [SerializeField, Range(0.1f, 0.8f)] private float effectDelay = 0.5f;
+    [SerializeField, Range(0.1f, 0.8f)] private float effectDelayEnter = 0.5f;
+    [SerializeField, Range(0.1f, 0.8f)] private float effectDelayExit = 0.5f;
     
     private IEnumerator OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Player")) yield break;
         // wait for a bit so that the player wont get stuck in switching directions every frame.
-        yield return new WaitForSeconds(effectDelay);
+        yield return new WaitForSeconds(effectDelayEnter);
         // multiplying by -1 to invert the speed/inputs
         other.GetComponent<PlayerMovement>().SetSpeedModifier(-1);
     }
@@ -19,8 +20,25 @@ public class DizzyingCauldronHazard : MonoBehaviour
     {
         if (!other.CompareTag("Player")) yield break;
         // wait for a bit so that the player wont get stuck in switching directions every frame.
-        yield return new WaitForSeconds(effectDelay);
+        yield return new WaitForSeconds(effectDelayExit);
         // return the speed modifier to normal
         other.GetComponent<PlayerMovement>().SetSpeedModifier(1);
+    }
+
+    public void SetEffectDelayEnter(float delay)
+    {
+        effectDelayEnter = delay;
+    }
+    public void SetEffectDelayExit(float delay)
+    {
+        effectDelayExit = delay;
+    }
+    public float GetEffectDelayEnter()
+    {
+        return effectDelayEnter;
+    }
+    public float GetEffectDelayExit()
+    {
+        return effectDelayExit; 
     }
 }
