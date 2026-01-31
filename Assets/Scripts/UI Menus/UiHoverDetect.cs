016a69292c2297681a37e36adf0df0483732b373
Adi Zahavi <adidizahavi5@gmail.com>
2026-01-31 14:17:23 +0200

    DOTween and implementation start

---
+++ Assets/Scripts/UI Menus/UiHoverDetect.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(DOTweenAnimation))]
+public class HoverDetect : MonoBehaviour,
+    IPointerEnterHandler,
+    IPointerExitHandler
+{
+    private DOTweenAnimation _tweenAnimation;
+
+    private bool _isHovering;
+    
+    private void Awake()
+    {
+        _tweenAnimation = GetComponent<DOTweenAnimation>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (_isHovering) return;
+        _tweenAnimation.DORestart();
+        _tweenAnimation.DOPlay();
+        _isHovering = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (!_isHovering) return;
+        _tweenAnimation.DOPlayBackwards();
+        _tweenAnimation.DORestart();
+        _isHovering = false;
+    }
+}
